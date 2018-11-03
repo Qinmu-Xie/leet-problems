790116ac090ea31ca2a9748a39346e81e1e993e0
qmx <xieqinmu@126.com>
2018-11-03 14:13:16 +0800

    add longestPalindrome

--- src/main/java/com/qmx/leet/problems/Solution.java
+++ src/main/java/com/qmx/leet/problems/Solution.java
@@ -1,10 +1,27 @@
 package com.qmx.leet.problems;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+
+class Memoizer<T, U> {
+
+    private final Map<T, U> cache = new ConcurrentHashMap<>();
+
+    private Memoizer() {
+    }
+
+    private Function<T, U> doMemoize(final Function<T, U> function) {
+        return input -> cache.computeIfAbsent(input, function::apply);
+    }
+
+    public static <T, U> Function<T, U> memoize(final Function<T, U> function) {
+        return new Memoizer<T, U>().doMemoize(function);
+    }
+}
 
 class Solution {
+
     public int[] twoSum(int[] nums, int target) {
 
         for (int i = 0; i < nums.length; i++) {
@@ -42,7 +59,7 @@ class Solution {
         return res.next;
     }
 
-    public int lengthOfLongestSubstring(String s){
+    public int lengthOfLongestSubstring(String s) {
         char[] chars = s.toCharArray();
         if (chars.length == 0)
             return 0;
@@ -72,10 +89,130 @@ class Solution {
             } else {
                 set.remove(s.charAt(i++));
             }
-            System.out.println("i: " + i + " j: " + j + " set: ");
+            System.out.println("i: " + i + " j: " + j + " j-i: " + (j - i));
             set.forEach(System.out::println);
         }
         return ans;
+    }
+
+    public int lengthOfLongestSubstring3(String s) {
+        int n = s.length(), ans = 0;
+        int[] index = new int[128]; // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            i = Math.max(index[s.charAt(j)], i);
+            ans = Math.max(ans, j - i + 1);
+            index[s.charAt(j)] = j + 1;
+        }
+        return ans;
+    }
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int total = nums1.length + nums2.length;
+        int[] takes = new int[(total / 2) + 1];
+        for (int i = 0, j = 0, cnt = 0; cnt < takes.length; cnt++) {
+            if (i >= nums1.length) {
+                takes[cnt] = nums2[j++];
+            } else if (j >= nums2.length) {
+                takes[cnt] = nums1[i++];
+            } else if (nums1[i] < nums2[j]) {
+                takes[cnt] = nums1[i++];
+            } else {
+                takes[cnt] = nums2[j++];
+            }
+        }
+        if (total % 2 == 0) {
+            return (takes[takes.length - 1] + takes[takes.length - 2]) / 2.0;
+        } else {
+            return takes[takes.length - 1];
+        }
+    }
+
+    public String longestPalindromeLongTime(String s) {
+        if (Objects.isNull(s) || "".equals(s) || s.length() == 1) {
+            return s;
+        }
+        String res = "";
+        int max = 0;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = s.length() - 1; i < j + 1; j--) {
+                if (isPalindrome(s, i, j)) {
+                    if (j - i + 1 > max) {
+                        max = j - i + 1;
+                        res = s.substring(i, j + 1);
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+//    Function<String, Boolean> IsPalindromeCached = Memoizer.memoize(this::isPalindrome);
+
+    public String longestPalindrome2(String s) {
+        int windowLength = s.length();
+        while (windowLength > 0) {
+            for (int i = 0; i < s.length() - windowLength + 1; i++) {
+                if (isPalindrome(s, i, i + windowLength - 1)) {
+                    return s.substring(i, i + windowLength);
+                }
+            }
+            windowLength--;
+        }
+        return "";
+    }
+
+    Boolean isPalindrome(String s, int i, int j) {
+        if (i == j) {
+            return true;
+        }
+        if (j == i + 1) {
+            return s.charAt(i) == s.charAt(j);
+        }
+        return s.charAt(i) == s.charAt(j)
+                && isPalindrome(s, i + 1, j - 1);
+    }
+
+    public String expandByCenter1(String s, int i, int j) {
+        int n = s.length();
+        while (i >= 0 && j <= n - 1 && s.charAt(i) == s.charAt(j)) {
+            i--;
+            j++;
+        }
+        return s.substring(i + 1, j);
+    }
+
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        if (n == 0 || n == 1) {
+            return s;
+        }
+        int start = 0, end = 0;
+        for (int i = 0; i < n; i++) {
+            int len1 = expandByCenter(s, i, i);
+            int len2 = expandByCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+
+        }
+        return s.substring(start, end + 1);
+    }
+
+
+    public int expandByCenter(String s, int left, int right) {
+        int l = left, r = right;
+        while (l >= 0 && r <= s.length() - 1 && s.charAt(l) == s.charAt(r)) {
+            l--;
+            r++;
+        }
+        return r - l - 1;
+    }
+
+    public String convert(String s, int numRows) {
+        return "";
     }
 
 }
--- src/test/java/com/qmx/leet/problems/SolutionTest.java
+++ src/test/java/com/qmx/leet/problems/SolutionTest.java
@@ -41,7 +41,34 @@ public class SolutionTest {
     }
 
     @Test
-    public void test() {
-        System.out.println("abcde".indexOf('c',2));
+    public void findMedianSortedArrays() {
+        int[] num1 = {1, 3};
+        int[] num2 = {2};
+        int[] num3 = {1, 2};
+        int[] num4 = {3, 4};
+        int[] num5 = {3};
+        int[] num6 = {-2, -1};
+        assertEquals(2.0, solution.findMedianSortedArrays(num1, num2), 0.00005);
+        assertEquals(2.5, solution.findMedianSortedArrays(num3, num4), 0.00005);
+        assertEquals(-1.0, solution.findMedianSortedArrays(num5, num6), 0.00005);
+    }
+
+    @Test
+    public void longestPalindrome() {
+        assertEquals("sfeefs", solution.expandByCenter1("dsfeefsfss", 3, 4));
+        assertEquals(6, solution.expandByCenter("dsfeefsfss", 3, 4));
+        assertEquals("sfeefs", solution.expandByCenter1("dsfeefsfss", 3, 4));
+        assertEquals(6, solution.expandByCenter("dsfeefsfss", 3, 4));
+        assertEquals("d", solution.expandByCenter1("dssdfeefdssfss", 3, 3));
+//        assertEquals("bab", solution.longestPalindrome("babad"));
+        assertEquals("bb", solution.longestPalindrome("cbbd"));
+        assertEquals("b", solution.longestPalindrome("b"));
+        assertEquals("ranynar", solution.longestPalindrome("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"));
+    }
+
+    @Test
+    public void convert() {
+        assertEquals("PAHNAPLSIIGYIR",solution.convert("PAYPALISHIRING",3));
+        assertEquals("PINALSIGYAHRPI",solution.convert("PAYPALISHIRING",4));
     }
 }
